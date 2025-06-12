f660ee412437bfe7c73b8f736f9f8416073023ec
Ananto Joyoadikusumo <gg.ananto@gmail.com>
2025-06-12 15:36:47 +0700

    Removed basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: "/anantoj.github.io",
-  trailingSlash: true, // Add this line
   eslint: {
     ignoreDuringBuilds: true,
   },
